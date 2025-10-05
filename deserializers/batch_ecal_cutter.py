210ab13cbe6b6b44f02711b8373dcca2bdb5650d
DenDen047 <sh.mn.nat@gmail.com>
2025-10-06 07:06:11 +0900

    ckpt: 2022-12-04/Morning/2022-12-01_15-46-09.009_wildpose_v1.1

--- deserializers/batch_ecal_cutter.py
+++ deserializers/batch_ecal_cutter.py
@@ -94,9 +94,14 @@ JOBS: List[CutterJob] = [
     #     output_root="/mnt/data/WildPose_v1.1//snippet_000",
     # ),
     CutterJob(
-        input_path="/media/ikuta/Expansion/2022-12-04/Morning/2022-12-01_15-20-48.388_wildpose_v1.1",
-        range=CutRange(start_seconds=0, end_seconds=38),
-        output_root="/mnt/data/WildPose_v1.1/Wildebeest/2022-12-04_001",
+        input_path="/media/ikuta/Expansion/2022-12-04/Morning/2022-12-01_15-46-09.009_wildpose_v1.1",
+        range=CutRange(start_seconds=46, end_seconds=55),
+        output_root="/mnt/data/WildPose_v1.1/Secretarybird/2022-12-04_001",
+    ),
+    CutterJob(
+        input_path="/media/ikuta/Expansion/2022-12-04/Morning/2022-12-01_15-46-09.009_wildpose_v1.1",
+        range=CutRange(start_seconds=64, end_seconds=77),
+        output_root="/mnt/data/WildPose_v1.1/Secretarybird/2022-12-04_002",
     ),
 ]
 
